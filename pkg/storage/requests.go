ac6da7eed65fba31e73dc403cba4171190fee9d5
Gleb Gonchar <gelerum@gmail.com>
2021-11-20 16:11:18 +0700

    update

--- pkg/storage/requests.go
+++ pkg/storage/requests.go
@@ -27,6 +27,7 @@ func (c client) AddPosition(chatID int, amount float64, category string, kind st
 func (c client) GetData(chatID int) {
 	coll := c.client.Database(os.Getenv("DATABASE_NAME")).Collection(os.Getenv("USED_COLLECTION"))
 	var podcast bson.D
-	coll.FindOne(context.TODO(), bson.D{{"chatID", chatID}}).Decode(&podcast)
+	err := coll.FindOne(context.TODO(), bson.D{{"chatID", chatID}}).Decode(&podcast)
+	fmt.Print(err)
 	fmt.Print(podcast, "podcast")
 }
