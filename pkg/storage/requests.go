05b837fab15c25ef2739616cac6f836769a598fe
Gleb Gonchar <gelerum@gmail.com>
2021-11-24 16:43:52 +0700

    update

--- pkg/storage/requests.go
+++ pkg/storage/requests.go
@@ -3,14 +3,16 @@ package storage
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type positions struct {
-	Category string  `bson:"category"`
-	Amount   float32 `bson:"amount"`
+	Category  string             `bson:"category"`
+	Amount    float32            `bson:"amount"`
+	CreatedAt primitive.DateTime `bson:"created_at"`
 }
 
 type user struct {
@@ -29,6 +31,7 @@ func (c client) Get(chatID int) {
 	fmt.Println(a.Expenses[1])
 	fmt.Println(a.Expenses[1].Category)
 	fmt.Println(a.Expenses[1].Amount)
+	fmt.Println(a.Expenses[1].CreatedAt)
 }
 
 func (c client) CreateUserDocument(chatID int) error {
@@ -45,7 +48,7 @@ func (c client) CreateUserDocument(chatID int) error {
 
 func (c client) AddTransaction(chatID int, amount float64, category string, kind string) error {
 	filter := bson.D{{"chatID", chatID}}
-	update := bson.M{"$push": bson.M{kind: bson.D{{"category", category}, {"amount", amount}}}}
+	update := bson.M{"$push": bson.M{kind: bson.D{{"category", category}, {"amount", amount}, {"created_at", time.Now()}}}}
 	_, err := c.Coll.UpdateOne(context.TODO(), filter, update)
 	return err
 }
