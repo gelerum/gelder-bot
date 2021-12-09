a58b84f95935c8dca8e572735b884a1b3679e973
Gleb Gonchar <gelerum@gmail.com>
2021-12-09 12:58:48 +0700

    update

--- internal/storage/requests_test.go
+++ internal/storage/requests_test.go
@@ -9,13 +9,22 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 )
 
-func TestGetTransactions(t *testing.T) {
+func initClient() (*Client, error) {
 	cfg := config.Client{
 		URI:        os.Getenv("MONGO_URI"),
 		Name:       os.Getenv("DATABASE_NAME"),
 		Collection: os.Getenv("DATABASE_TEST_COLLECTION"),
 	}
-	client, _ := NewClient(&cfg)
+	c, err := NewClient(&cfg)
+	return c, err
+}
+
+func TestGetTransactions(t *testing.T) {
+	c, err := initClient()
+	if err != nil {
+		t.Error(err)
+		return
+	}
 	expenseDate, _ := time.Parse("2006-01-02 15:04:05 -0700 MST", "2021-12-08 13:56:55.362 +0000 UTC")
 	expectedExpense := Transaction{
 		Category:     "food",
@@ -28,26 +37,24 @@ func TestGetTransactions(t *testing.T) {
 		Amount:       20,
 		CreationDate: incomeDate,
 	}
-	expenses, income := client.GetTransactions(1)
+	expenses, income := c.GetTransactions(1)
 	if expenses[0] != expectedExpense || income[0] != expectedIncome {
 		t.Error("GetTransactions(1) =", expenses, income)
 	}
-	expenses, income = client.GetTransactions(0)
+	expenses, income = c.GetTransactions(0)
 	if expenses != nil || income != nil {
 		t.Error("GetTransactions(0) =", expenses, income)
 	}
 }
 
 func TestCreateUserDocument(t *testing.T) {
-	cfg := config.Client{
-		URI:        os.Getenv("MONGO_URI"),
-		Name:       os.Getenv("DATABASE_NAME"),
-		Collection: os.Getenv("DATABASE_TEST_COLLECTION"),
+	c, err := initClient()
+	if err != nil {
+		t.Error(err)
+		return
 	}
-	c, _ := NewClient(&cfg)
-
 	// first case
-	err := c.CreateUserDocument(1)
+	err = c.CreateUserDocument(1)
 	if err != nil {
 		t.Error("client.CreateUserDocument(1) =", err)
 		return
@@ -92,4 +99,13 @@ func TestCreateUserDocument(t *testing.T) {
 		t.Error("Collection wasn't returned to original state with delete added document")
 		return
 	}
+}
+
+func TestAddTransaction(t *testing.T) {
+	c, err := initClient()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	err = c.AddTransaction(1, 10, "food", "expenses")
 }
