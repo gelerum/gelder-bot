d191863723e09485341d5461bb74e57ce1edace8
Gleb Gonchar <gelerum@gmail.com>
2021-12-09 11:02:46 +0700

    update

--- internal/storage/client_test.go
+++ internal/storage/client_test.go
@@ -1,6 +1,7 @@
 package storage
 
 import (
+	"fmt"
 	"os"
 	"testing"
 
@@ -13,6 +14,7 @@ func TestNewClient(t *testing.T) {
 		Name:       os.Getenv("DATABASE_NAME"),
 		Collection: os.Getenv("DATABASE_TEST_COLLECTION"),
 	}
+	fmt.Println(cfg.URI)
 	_, err := NewClient(&cfg)
 	if err != nil {
 		t.Error(err)
